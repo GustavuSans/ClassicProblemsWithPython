524c1e14decf1c127499dacba787ecfe1203ec40
Gustavu <gustavusans@gmail.com>
2025-05-16 19:39:35 -0300

    Melhorando a validação de dados com expressão regular

--- data_validation.py
+++ data_validation.py
@@ -1,12 +1,21 @@
+import re
+
 def validation():
-    text = input("Informe um texto: ")
-    hasNumber = False
-    for char in text:
-        if char.isdigit():
-            hasNumber = True
-            break
-    if hasNumber:
-        print("A string contém pelo menos um número.")
+    print("Observação: O nome não pode conter números ou caracteres especiais.")
+    print("Se houver serão removidos.")
+    name = input("Informe se nome: ")
+    clean_name = re.sub(r'[^A-Za-zÀ-ÿ\s]', '', name)
+    newName = ''.join(clean_name)
+    removed_chars = [char for char in name if char not in clean_name]
+    del clean_name
+    if(removed_chars):
+        print(f"Items removidos: {removed_chars}")
     else:
-        print("A string não contém números.")
+        print("Nenhum caractere inválido foi encontrado")
+    if(newName == ''):
+        print("Nome vázio")
+        del newName
+    else:
+        print(f"Nome: {newName}")
+
 validation()
