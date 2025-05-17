45607acc65e74a7323c4ef020ef55be595e315d1
Gustavu <gustavusans@gmail.com>
2025-05-17 16:37:06 -0300

    Criptografia One-Time Pad (OTP)

--- source/crypto.py
+++ source/crypto.py
@@ -1,6 +1,21 @@
 from secrets import token_bytes
 from typing import Tuple
 
-def key(lenght: int) -> int:
+def getKey(lenght: int) -> int:
     tb: bytes = token_bytes(lenght)
     return int.from_bytes(tb, 'big')
+def encrypt(original: str) -> Tuple[int, int]:
+    original_bytes : bytes = original.encode()
+    dummy : int = getKey(len(original_bytes))
+    original_key : int = int.from_bytes(original_bytes, 'big')
+    encrypted : int = original_key ^ dummy
+    return dummy, encrypted
+def decrypt(key_1: int, key_2: int) -> str:
+    decrypted : int = key_1 ^ key_2
+    temp : bytes = decrypted.to_bytes((decrypted.bit_length() + 7) // 8, 'big')
+    return temp.decode()
+
+key_1, key_2 = encrypt('secredo')
+print(key_1, key_2)
+result: str = decrypt(key_1, key_2)
+print(result)
