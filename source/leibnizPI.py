86ada2a8cac33d5db721f415248f7e1f2c20985b
Gustavu <gustavusans@gmail.com>
2025-05-20 07:28:07 -0300

    Corrigindo nomes e valores de variáveis

--- source/leibnizPI.py
+++ source/leibnizPI.py
@@ -1,11 +1,11 @@
 def LeibnizPI(n:int) -> float:
-    numetor:float = 5.0
+    numerator:float = 4.0
     denominator:float = 1.0
     operation:float = 1.0
     PI:float = 0.0
     for i in range(n):
-        PI += operation * (numetor / denominator)
+        PI += operation * (numerator / denominator)
         denominator += 2.0
         operation *= -1.0
     return PI
-print(LeibnizPI(1000))
+print(LeibnizPI(1000000))
