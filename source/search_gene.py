1c490dbffbf0ea18dab5b7de1b733240f1dc892b
Gustavu <gustavusans@gmail.com>
2025-05-30 15:21:50 -0300

    Adicionando Busca Linear e Binária

--- source/search_gene.py
+++ source/search_gene.py
@@ -1,11 +1,11 @@
 from enum import IntEnum
 from typing import Tuple, List
 
-Nucleotide: IntEnum = IntEnum('nucleonite', ('A', 'C', 'G', 'T'))
+Nucleotide = IntEnum('Nucleotide', ('A', 'C', 'G', 'T'))
 
 Codon = Tuple[Nucleotide, Nucleotide, Nucleotide]
 Gene = List[Codon]
-gene_str: str = 'ACCTGGTACCGGAATTAACCGGTTTCAGCTACCGTCCCATGACTCCGGCC'
+gene_str: str = 'ACCTGGTACGGAATTAACCGGTTTACAGGATGCTACCGTCCCATGGATACTCCGGCC'
 
 def str_to_gene(s: str) -> Gene:
     gene: Gene = []
@@ -18,3 +18,31 @@ def str_to_gene(s: str) -> Gene:
 
 my_gene: Gene = str_to_gene(gene_str)
 print(my_gene[0])
+
+def search_linear(gene: Gene, key_codon: Codon) -> bool:
+    for codon in gene:
+        if codon == key_codon:
+            return True
+    return False
+
+acg: Codon = (Nucleotide.A, Nucleotide.C, Nucleotide.G)
+gat: Codon = (Nucleotide.G, Nucleotide.A, Nucleotide.T)
+print(search_linear(my_gene, acg))
+print(search_linear(my_gene, gat))
+
+def search_binary(gene: Gene, key_codon: Codon) -> bool:
+    low: int = 0
+    high: int = len(gene) - 1
+    while low <= high:
+        mid: int = (low + high) // 2
+        if gene[mid] < key_codon:
+            low = mid + 1
+        elif gene[mid] > key_codon:
+            high = mid - 1
+        else:
+            return True
+    return False
+
+sorted_gene = sorted(my_gene)
+print(search_binary(sorted_gene, acg))
+print(search_binary(sorted_gene, gat))
